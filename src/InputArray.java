77fab3567e2478e37ad64c68dd5766f940378a4a
unknown <michelleherman13@gmail.com>
2022-02-20 19:49:34 -0600

    add

--- src/InputArray.java
+++ src/InputArray.java
@@ -7,12 +7,17 @@ public class InputArray {
         Scanner sc = new Scanner(System.in);
 
         for (int i=0; i < names.length; i++) {
-            System.out.print("User Input Here: ");
+            System.out.print("Enter Name Here: ");
             String input = sc.nextLine();
             names[i]=input;
         }
         for (String n:names) {
             System.out.println(n);
+            if (n.equals("name")) { //this example shows how to exit the loop once you meet a condition
+                break;              //for example, if the user inputs "name", then nothing else will be printed
+                                    //after the first time name is printed out (loop exited).
+
+            }
         }
     }
 }
