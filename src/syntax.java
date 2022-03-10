caa21015fc967981d2399b9ef24a67dab79d7cf0
michelleherman13 <85762953+michelleherman13@users.noreply.github.com>
2022-03-09 22:27:18 -0600

    Create syntax.java

---
+++ src/syntax.java
@@ -0,0 +1,13 @@
+// syntax notes on getters and setters 
+  
+  //getter syntax! notes for my own benefit
+    public String getUsername(){ //public accessor to get private variable
+      return this.username;
+    }
+
+
+
+    //setter syntax (if you want to change a private variable)
+    public void setUsername(String username){ //takes old username and updates it with passed in params 
+      this.username=username;
+    }
